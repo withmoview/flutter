d593596a3719c0603809d4b9b2162283fab00ee5
bana1127 <jaeson1012@naver.com>
2026-01-14 21:06:51 +0900

    11

--- minix_flutter/lib/screens/shell/home_shell.dart
+++ minix_flutter/lib/screens/shell/home_shell.dart
@@ -55,7 +55,7 @@ class _HomeShellState extends State<HomeShell> {
                 items: _items,
                 index: _index,
                 onChanged: (i) => setState(() => _index = i),
-                onAiTap: () => Get.toNamed('/compose'),
+                onAiTap: () => Get.toNamed('/ai'),
               ),
             ),
           ),
